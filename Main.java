6a996e19b83531db70f9b0d12a3578829b7eb908
Tarun Dev <tarun.dev@archa.com.au>
2021-08-09 01:46:18 +1000

    Fix bug in negative logic and add test cases for negative cases

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Digits{
 
-  static int[] stringToIntArray(String strDigits){
+  static Integer[] stringToIntArray(String strDigits){
     String firstDigit = strDigits.substring(0, 1);
     int sliceStartIndex = 1;
     if(strDigits.charAt(0) == '-'){
@@ -11,114 +12,130 @@ class Digits{
     }
     strDigits = strDigits.substring(sliceStartIndex);
     char[] charDigits = strDigits.toCharArray();
-     int size = charDigits.length + 1;
-     int[] arr = new int[size];
-     arr[0] = Integer.parseInt(firstDigit);
-     for(int i=1; i<size; i++) {
-        arr[i] = Character.getNumericValue(charDigits[i-1]);
-     }
-     return arr;
+    int size = charDigits.length + 1;
+    Integer[] arr = new Integer[size];
+    arr[0] = Integer.parseInt(firstDigit);
+    for(int i=1; i<size; i++) {
+      arr[i] = Character.getNumericValue(charDigits[i-1]);
+    }
+    return arr;
   }
 
-    static int[] swapValue(int[] arr, int fstIndex, int sndIndex){
-        int temp = arr[fstIndex];
-        arr[fstIndex] = arr[sndIndex];
-        arr[sndIndex] = temp;
-        return arr;
+  static Integer[] swapValue(Integer[] arr, int fstIndex, int sndIndex){
+    int temp = arr[fstIndex];
+    arr[fstIndex] = arr[sndIndex];
+    arr[sndIndex] = temp;
+    return arr;
+  }
+
+  String sameDigitNextHighestValue(String strDigits){
+    Integer[] digitsArr = stringToIntArray(strDigits);
+    boolean negative = false;
+    if(digitsArr[0] < 0){
+      digitsArr[0] = digitsArr[0] * -1;
+      negative = true;
     }
+    int digitsLen = digitsArr.length;
+    int prevDigit = digitsArr[digitsLen-1];
+    int[] digitsSortPart;
+    int sliceIndex = -1;
+    String finalDigits = "";
 
-    String sameDigitNextHighestValue(String strDigits){
-        int[] digitsArr = stringToIntArray(strDigits);
-        int digitsLen = digitsArr.length;
-        int prevDigit = digitsArr[digitsLen-1];
-        int[] digitsSortPart;
-        int sliceIndex = -1;
-        boolean negative = false;
+    for(int i=digitsLen-2; i>=0; i--){
 
-        if(digitsArr[0] < 0){
-          digitsArr[0] = digitsArr[0] * -1;
-          negative = true;
+      int currDigit = digitsArr[i];
+      if(negative){
+        if(currDigit > prevDigit){
+          sliceIndex = i;
+          break;
         }
-
-        for(int i=digitsLen-2; i>=0; i--){
-
-            int currDigit = digitsArr[i];
-            if(negative){
-              if(currDigit > prevDigit){
-                  sliceIndex = i;
-                  break;
-              }
-            }else{
-              if(currDigit < prevDigit){
-                  sliceIndex = i;
-                  break;
-              }
-            }
-
-            prevDigit = currDigit;
-
+      }else{
+        if(currDigit < prevDigit){
+          sliceIndex = i;
+          break;
         }
-        if(sliceIndex == -1)
-            return "-1";
+      }
 
-        int swapIndex = sliceIndex+1;
-        // System.out.println("swapIndex");
-        // System.out.println(swapIndex);
-        // System.out.println("digitsLen");
-        // System.out.println(digitsLen);
-        for(int i=sliceIndex+2; i<digitsLen;i++){
-            if(digitsArr[i] < digitsArr[swapIndex])
-                swapIndex = i;
-        }
-        // System.out.println("Before swap");
-        // System.out.println(Arrays.toString(digitsArr));
-        digitsArr = swapValue(digitsArr, sliceIndex, swapIndex);
-        // System.out.println("After swap");
-        // System.out.println(Arrays.toString(digitsArr));
-
-        // System.out.println();
-        // System.out.println("swapIndex");
-        // System.out.println(swapIndex);
-        // System.out.println("sliceIndex");
-        // System.out.println(sliceIndex+1);
-        // System.out.println("digitsLen");
-        // System.out.println(digitsLen);
-        // System.out.println(digitsArr[sliceIndex+1]);
-        Arrays.sort(digitsArr, sliceIndex+1, digitsLen);
-        // System.out.println("After sort");
-        // System.out.println(Arrays.toString(digitsArr));
-
-        // digitsSortPart = Arrays.copyOfRange(digitsArr, sliceIndex, digitsLen);
-
-         // System.out.println("Final val");
-        // System.out.println(Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", ""));
-        if(negative)
-          return "-" + Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", "");
-
-        return Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", "");
-
+      prevDigit = currDigit;
 
     }
+    if(sliceIndex == -1)
+    return "No output";
+
+    int swapIndex = sliceIndex+1;
+    // System.out.println("swapIndex");
+    // System.out.println(swapIndex);
+    // System.out.println("digitsLen");
+    // System.out.println(digitsLen);
+    for(int i=sliceIndex+2; i<digitsLen;i++){
+      if(negative){
+        if(digitsArr[i] > digitsArr[swapIndex] && digitsArr[i] < digitsArr[sliceIndex])
+          swapIndex = i;
+      }else{
+        if(digitsArr[i] < digitsArr[swapIndex])
+          swapIndex = i;
+      }
+    }
+    // System.out.println("Before swap");
+    // System.out.println(Arrays.toString(digitsArr));
+    digitsArr = swapValue(digitsArr, sliceIndex, swapIndex);
+    // System.out.println("After swap");
+    // System.out.println(Arrays.toString(digitsArr));
+
+    // System.out.println();
+    System.out.println("swapIndex");
+    System.out.println(swapIndex);
+    System.out.println("sliceIndex");
+    System.out.println(sliceIndex);
+    // System.out.println("digitsLen");
+    // System.out.println(digitsLen);
+    // System.out.println(digitsArr[sliceIndex+1]);
+    if(negative){
+      System.out.println("Before sort");
+      System.out.println(Arrays.toString(digitsArr));
+      Arrays.sort(digitsArr, sliceIndex+1, digitsLen, Collections.reverseOrder());
+      System.out.println("After sort");
+      System.out.println(Arrays.toString(digitsArr));
+    }else{
+      Arrays.sort(digitsArr, sliceIndex+1, digitsLen);
+    }
+
+    // System.out.println("After sort");
+    // System.out.println(Arrays.toString(digitsArr));
+
+    // digitsSortPart = Arrays.copyOfRange(digitsArr, sliceIndex, digitsLen);
+
+    // System.out.println("Final val");
+    // System.out.println(Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", ""));
+    finalDigits = Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", "");
+    if(negative)
+    finalDigits = "-" + Arrays.toString(digitsArr).replaceAll("\\[|\\]|,|\\s", "");
+
+    // return "" + Integer.parseInt(finalDigits);
+    return finalDigits;
+
+
+  }
 
 }
 
 public class Main{
 
-     public static void main(String []args){
-        //  String str = "12345";
-        //  String str = "218765";
-        //  String str = "1234";
-        //  String str = "4321";
-         // String str = "534976";
-         // String str = args[0];
-         System.out.println("Your first argument is: "+args[0]);
-         // char[] charDigits = str.toCharArray();
-         //  int size = charDigits.length;
-         //  int [] arr = new int[size];
-         //  for(int i=0; i<size; i++) {
-         //     arr[i] = Character.getNumericValue(charDigits[i]);
-         //  }
-          Digits d = new Digits();
-        System.out.println(d.sameDigitNextHighestValue(args[0]));
-     }
+  public static void main(String []args){
+    //  String str = "12345";
+    //  String str = "218765";
+    //  String str = "1234";
+    //  String str = "4321";
+    // String str = "534976";
+    // String str = args[0];
+    System.out.println("Your first argument is: "+args[0]);
+    // char[] charDigits = str.toCharArray();
+    //  int size = charDigits.length;
+    //  int [] arr = new int[size];
+    //  for(int i=0; i<size; i++) {
+    //     arr[i] = Character.getNumericValue(charDigits[i]);
+    //  }
+    Digits d = new Digits();
+    System.out.println(d.sameDigitNextHighestValue(args[0]));
+  }
 }
--- TestDigits.java
+++ TestDigits.java
@@ -3,58 +3,102 @@ import static org.junit.Assert.assertEquals;
 
 public class TestDigits {
 
-   @Test
-   public void testSixDigits() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("534976");
-     assertEquals("536479", result);
-   }
+  @Test
+  public void testTwoDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("35");
+    assertEquals("53", result);
+  }
 
-   @Test
-   public void testTwoDigits() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("35");
-     assertEquals("53", result);
-   }
+  @Test
+  public void testSixDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("534976");
+    assertEquals("536479", result);
+  }
 
-   @Test
-   public void testDigitsNoOutput() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("54321");
-     assertEquals("-1", result);
-   }
+  @Test
+  public void testSevenDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("1000006");
+    assertEquals("1000060", result);
+  }
 
-   @Test
-   public void testDigitsWith() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("1000006");
-     assertEquals("1000060", result);
-   }
+  @Test
+  public void testDigitsNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("54321");
+    assertEquals("No output", result);
+  }
 
-   @Test
-   public void testSameDigitsNoOutput() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("111");
-     assertEquals("-1", result);
-   }
+  @Test
+  public void testDigitsWithOnlyZeroNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("0000000");
+    assertEquals("No output", result);
+  }
 
-   @Test
-   public void testZeroDigitsNoOutput() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("100000");
-     assertEquals("-1", result);
-   }
+  @Test
+  public void testSameDigitsNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("111");
+    assertEquals("No output", result);
+  }
 
-   @Test
-   public void testNegativeDigitsNoOutput() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("-123");
-     assertEquals("-1", result);
-   }
-   @Test
-   public void testNegativeDigits() {
-     Digits d = new Digits();
-     String result = d.sameDigitNextHighestValue("-321");
-     assertEquals("-312", result);
-   }
+
+  @Test
+  public void testZeroDigitsNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("100000");
+    assertEquals("No output", result);
+  }
+
+  @Test
+  public void testNegativeTwoDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-61");
+    assertEquals("-16", result);
+  }
+
+  @Test
+  public void testNegativeSixDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-297501");
+    assertEquals("-297105", result);
+  }
+
+  @Test
+  public void testNegativeSevenDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-9000006");
+    assertEquals("-6000009", result);
+  }
+
+  @Test
+  public void testNegativeSevenDigits2() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-1000006");
+    assertEquals("-0610000", result);
+  }
+
+  @Test
+  public void testNegativeDigitsNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-123");
+    assertEquals("No output", result);
+  }
+
+  @Test
+  public void testNegativeDigits() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-321");
+    assertEquals("-312", result);
+  }
+
+  @Test
+  public void testNegativeDigitsWithOnlyZeroNoOutput() {
+    Digits d = new Digits();
+    String result = d.sameDigitNextHighestValue("-0000000");
+    assertEquals("No output", result);
+  }
 }
